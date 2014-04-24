90bf96141edce34d9f52ece1a818f0912e892a61
ZY_android <zhangyang9036@gmail.com>
2014-04-23 20:41:02 -0700

    listview implemented

--- Pickit/gen/com/example/govege/R.java
+++ Pickit/gen/com/example/govege/R.java
@@ -982,10 +982,12 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int cross=0x7f020057;
-        public static final int frontpage=0x7f020058;
-        public static final int ic_launcher=0x7f020059;
-        public static final int magnifying_glass=0x7f02005a;
+        public static final int apple=0x7f020057;
+        public static final int background_shape=0x7f020058;
+        public static final int cross=0x7f020059;
+        public static final int frontpage=0x7f02005a;
+        public static final int ic_launcher=0x7f02005b;
+        public static final int magnifying_glass=0x7f02005c;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -999,7 +1001,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050046;
+        public static final int action_settings=0x7f050049;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1034,7 +1036,6 @@ containing a value of this type.
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
-        public static final int searchBar=0x7f050045;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1052,10 +1053,14 @@ containing a value of this type.
         public static final int showTitle=0x7f050006;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
+        public static final int suggest_list=0x7f050045;
+        public static final int suggest_list_pic=0x7f050046;
         public static final int tabMode=0x7f050002;
         public static final int tap_strip_title=0x7f050041;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
+        public static final int tv_suggest_item_amount=0x7f050048;
+        public static final int tv_suggest_item_price=0x7f050047;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
@@ -1113,11 +1118,9 @@ containing a value of this type.
         public static final int activity_loading=0x7f030018;
         public static final int activity_main=0x7f030019;
         public static final int activity_search=0x7f03001a;
-        public static final int fragment_loading=0x7f03001b;
-        public static final int fragment_main=0x7f03001c;
-        public static final int fragment_search=0x7f03001d;
-        public static final int main_fragment=0x7f03001e;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001f;
+        public static final int main_fragment=0x7f03001b;
+        public static final int suggest_item=0x7f03001c;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
     }
     public static final class menu {
         public static final int loading=0x7f0c0000;
--- Pickit/src/pagefragment/MainPageFragment.java
+++ Pickit/src/pagefragment/MainPageFragment.java
@@ -1,14 +1,22 @@
 package pagefragment;
-import com.example.govege.R;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.example.govege.R;
 
 public class MainPageFragment extends Fragment {
+	
+	public static String[] fake = {"Apple", "Pear", "Banana"};
 	
 	public MainPageFragment newInstance(int sectionNum){
 		return new MainPageFragment();
@@ -42,11 +50,40 @@ public class MainPageFragment extends Fragment {
 
 			Bundle savedInstanceState) {
 
-		View rootView = inflater.inflate(R.layout.main_fragment, container,
-
-				false);
-
-
+		View rootView = inflater.inflate(R.layout.main_fragment, container,false);
+		ListView lv = (ListView) rootView.findViewById(R.id.suggest_list);
+		SimpleAdapter adapter = new SimpleAdapter(this.getActivity(),getData(),R.layout.suggest_item,
+				new String[]{"price", "amount", "img"},
+				new int[]{R.id.tv_suggest_item_price,R.id.tv_suggest_item_amount,
+			R.id.suggest_list_pic});
+		
+		lv.setAdapter(adapter);
+	
 		return rootView;
+	}
+	
+	private List<Map<String, Object>> getData(){
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("price", "$50");
+		map.put("amount", "1,000");
+		map.put("img", R.drawable.apple);
+		list.add(map);
+
+		Map<String, Object> map1 = new HashMap<String, Object>();
+		map1.put("price", "$500");
+		map1.put("amount", "1,000");
+		map1.put("img", R.drawable.apple);
+		list.add(map1);
+		
+		Map<String, Object> map2 = new HashMap<String, Object>();
+		map2.put("price", "$3000");
+		map2.put("amount", "1,000");
+		map2.put("img", R.drawable.apple);
+		list.add(map2);
+		
+		return list;
+		
 	}
 }
